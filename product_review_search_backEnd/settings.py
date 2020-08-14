9fbd6539d5a93913bc40c6d23d7ec227c5bfd5e5
pr4shantS0naw4n3 <prashant.sonawane@18northsystems.com>
2020-08-14 22:30:02 +0530

    updated settings

--- product_review_search_backEnd/settings.py
+++ product_review_search_backEnd/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://product-review-search.herokuapp.com/']
+ALLOWED_HOSTS = ['product-review-search.herokuapp.com']
 
 # Application definition
 
